4d064a2c7d312cdde4b344920128ff52beb848ea
awakesay <awakesay@live.jp>
2023-09-30 15:35:56 +0900

    Big change

--- lib/util.py
+++ lib/util.py
@@ -1,9 +1,14 @@
 
 
 
-
 from pathlib import Path
+from typing import Final
 import json5
+
+APP_ROOT_PARENTS: Final[int] = 1
+
+def app_root(joinpath = '') -> Path:
+    return Path(__file__).parents[APP_ROOT_PARENTS].joinpath(joinpath)
 
 def get_bot_token() -> str:
     """./Config/discord.json5 からボットのトークンを読み出して返します。"""
@@ -12,6 +17,4 @@ def get_bot_token() -> str:
         return json5.load(f)['bot_token']
 
 if __name__ == '__main__':
-
-    bot_token = get_bot_token()
-    print(bot_token)
+    """Test code."""
